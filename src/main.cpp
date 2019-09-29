1a2ae7b85d28a5b36b196811379e24d4923105d6
mariano <mlapenna79@hotmail.com>
2019-09-28 22:45:49 -0300

    Emprolijo micro detalles en el main.

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main (int argc, char** argv) {
         if (archiConfigCarga.status == 0) {
             //Cargó bien el config por parámetro, no busca el config default
             cargarArchivoConfigPorDefecto = false;
-        } else{
+        } else {
             //No cargó bien el config por parámetro, buscará el config default
             Log::ObtenerInstancia()->Error("No se pudo abrir el archivo de configuración \"" + archiConfigPath + "\"."
                                            + " Se detectó el problema en el byte " + to_string(archiConfigCarga.offset)
@@ -28,7 +28,7 @@ int main (int argc, char** argv) {
     }
 
     //Ve si tiene que buscar el config por defecto
-    if(cargarArchivoConfigPorDefecto) {
+    if (cargarArchivoConfigPorDefecto) {
         archiConfigPath = "config/default.xml";
         archiConfigCarga = archiConfig.load_file(archiConfigPath.data());
         if (archiConfigCarga.status != 0) {
@@ -53,14 +53,14 @@ int main (int argc, char** argv) {
     }
 
     //Se inicializa el Logger acá y así queda para el resto de la aplicación.
-    if(!Log::InicializarLog(logLevel, "")) {
+    if (!Log::InicializarLog(logLevel, "")) {
         //Si ya se había inicializado el logger (por error en apertura de config) al menos le setea el nivel de log
         Log::ObtenerInstancia()->SetSeveridadMinima(logLevel);
     }
     //En cualquier clase que haya que utilizar el logger, se lo instancia así
     Log *logueador  =  Log::ObtenerInstancia();
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         logueador->Error("No se pudo iniciar SDL2 correctamente");
         return -1;
     }
