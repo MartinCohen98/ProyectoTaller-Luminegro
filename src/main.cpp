69fd4eead5fb453c472e87e902c40dd39bd2985f
mariano <mlapenna79@hotmail.com>
2019-09-28 23:15:55 -0300

    Vuelo variable archiConfigPath para usar directamente argv[x]. Incorporo un nuevo parámetro de arranque: cliente o servidor

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,56 @@ using namespace Logger;
 
 int main (int argc, char** argv) {
 
+    /** Parámetros de línea de comandos para ejecutar el juego:
+     *
+     * 1: "servidor" o "cliente" (OBLIGATORIO)
+     * 2: ubicación del archivo de configuración
+     * 3: modo de logueo
+     *
+     * Ejemplos:
+     *      ./ProyectoTaller-Luminegro servidor config/repiola.xml DEBUG
+     *      ./ProyectoTaller-Luminegro servidor
+     *
+     **/
+
     // Archivo de configuración
-    string archiConfigPath;
     pugi::xml_document archiConfig;
     pugi::xml_parse_result archiConfigCarga;
     bool cargarArchivoConfigPorDefecto = true;
 
     // Envió por parámetro la ubicación del archivo?
-    if (argc >= 2) {
+    if (argc >= 3) {
         // Si
-        archiConfigPath = argv[1];
-        archiConfigCarga = archiConfig.load_file( archiConfigPath.data());
+        archiConfigCarga = archiConfig.load_file(argv[2]);
+
         if (archiConfigCarga.status == 0) {
-            //Cargó bien el config por parámetro, no busca el config default
+            // Cargó bien el config por parámetro, no busca el config default
             cargarArchivoConfigPorDefecto = false;
+
         } else {
-            //No cargó bien el config por parámetro, buscará el config default
-            Log::ObtenerInstancia()->Error("No se pudo abrir el archivo de configuración \"" + archiConfigPath + "\"."
-                                           + " Se detectó el problema en el byte " + to_string(archiConfigCarga.offset)
-                                           + ". Mensaje de error: " + archiConfigCarga.description());
+            // No cargó bien el config por parámetro, buscará el config default
+            string mensajeError = "No se pudo abrir el archivo de configuración \"";
+
+            mensajeError.append(argv[2]);
+            mensajeError.append("\". Se detectó el problema en el byte " + to_string(archiConfigCarga.offset)
+                                  + ". Mensaje de error: " + archiConfigCarga.description());
+
+            Log::ObtenerInstancia()->Error(mensajeError);
         }
     }
 
-    //Ve si tiene que buscar el config por defecto
+    // Ver si tiene que buscar el config por defecto
     if (cargarArchivoConfigPorDefecto) {
-        archiConfigPath = "config/default.xml";
-        archiConfigCarga = archiConfig.load_file(archiConfigPath.data());
+        archiConfigCarga = archiConfig.load_file("config/default.xml");
+
         if (archiConfigCarga.status != 0) {
-            //Rompió hasta el config por defecto, más no se puede hacer
-            Log::ObtenerInstancia()->Error("No se pudo abrir el archivo de configuración por defecto: \"" + archiConfigPath + "\"."
-                                           + " Se detectó el problema en el byte " + to_string(archiConfigCarga.offset)
-                                           + ". Mensaje de error: " + archiConfigCarga.description());
+            // Rompió hasta el config por defecto, más no se puede hacer
+            string mensajeError = "No se pudo abrir el archivo de configuración por defecto: \"";
+            mensajeError.append(argv[2]);
+            mensajeError.append("\". Se detectó el problema en el byte " + to_string(archiConfigCarga.offset)
+                                + ". Mensaje de error: " + archiConfigCarga.description());
+
+            Log::ObtenerInstancia()->Error(mensajeError);
             return EXIT_FAILURE;
         }
     }
@@ -44,9 +63,9 @@ int main (int argc, char** argv) {
     string logLevel;
 
     // Lo envió por parámetro?
-    if (argc >= 3) {
+    if (argc >= 4) {
         // Si
-        logLevel = argv[2];
+        logLevel = argv[3];
     } else {
         // No, leo del XML
         logLevel = archiConfig.child("configuracion").child("log").child_value("level");
@@ -60,18 +79,27 @@ int main (int argc, char** argv) {
     //En cualquier clase que haya que utilizar el logger, se lo instancia así
     Log *logueador  =  Log::ObtenerInstancia();
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        logueador->Error("No se pudo iniciar SDL2 correctamente");
-        return -1;
+
+    // Arrancamos...
+    if (strcmp(argv[1], "servidor") == 0) {
+        logueador->Info("Se inicia el juego en modo servidor");
+
+    } else if (strcmp(argv[1], "cliente") == 0) {
+        logueador->Info("Se inicia el juego en modo cliente");
+
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            logueador->Error("No se pudo iniciar SDL2 correctamente");
+            return -1;
+        }
+
+        logueador->Info("SDL inicio correctamente, ejecutando...");
+
+        VentanaDeJuego ventana;
+
+        int retorno = ventana.abrir(&archiConfig);
+
+        logueador->Info("Fin del juego");
+
+        return retorno;
     }
-
-    logueador->Info("SDL inicio correctamente, ejecutando...");
-
-	VentanaDeJuego ventana;
-
-	int retorno = ventana.abrir(&archiConfig);
-
-    logueador->Info("Fin del juego");
-
-	return retorno;
 }
