798c23285ac4cd3cdf5e4e978e365cd59cc4f206
Martin <mrtincohen98@gmail.com>
2019-10-03 20:47:23 -0300

    Quitado de un comentario

--- src/VistaEnemigo.cpp
+++ src/VistaEnemigo.cpp
@@ -1,18 +1,6 @@
-/*
- * VistaEnemigo.cpp
- *
- *  Created on: Oct 3, 2019
- *      Author: martin
- */
-
 #include "VistaEnemigo.h"
 
-VistaEnemigo::VistaEnemigo() {
-	// TODO Auto-generated constructor stub
+VistaEnemigo::VistaEnemigo() {}
 
-}
-
-VistaEnemigo::~VistaEnemigo() {
-	// TODO Auto-generated destructor stub
-}
+VistaEnemigo::~VistaEnemigo() {}
 
