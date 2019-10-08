de0e273980d60b75e10dfd3f8f85fdc80064a464
julio <julio@julio-HP-240-G6-Notebook-PC>
2019-10-07 23:19:50 -0300

    Se lee la cantidad de jugadores del Config. Se crea la clase ControlJugadores.

---
+++ src/ControlJugadores.cpp
@@ -0,0 +1,18 @@
+/*
+ * ControlJugadores.cpp
+ *
+ *  Created on: 7 oct. 2019
+ *      Author: julio
+ */
+
+#include "ControlJugadores.h"
+
+ControlJugadores::ControlJugadores() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ControlJugadores::~ControlJugadores() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/ControlJugadores.h
@@ -0,0 +1,17 @@
+/*
+ * ControlJugadores.h
+ *
+ *  Created on: 7 oct. 2019
+ *      Author: julio
+ */
+
+#ifndef SRC_CONTROLJUGADORES_H_
+#define SRC_CONTROLJUGADORES_H_
+
+class ControlJugadores {
+public:
+	ControlJugadores();
+	virtual ~ControlJugadores();
+};
+
+#endif /* SRC_CONTROLJUGADORES_H_ */
