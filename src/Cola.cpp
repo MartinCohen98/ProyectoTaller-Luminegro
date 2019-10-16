9b90dae2aea723701daeacf1a0f163721049f85e
NicoSabbath <sallisnicolas@gmail.com>
2019-10-16 04:19:23 -0300

    Se agrega la cola genérica

---
+++ src/Cola.cpp
@@ -0,0 +1,70 @@
+#include "Cola.h"
+#include <stdexcept>
+
+
+template <typename T>
+Cola<T>::Cola() {
+    this->ultimoNodo = nullptr;
+    this->primerNodo = nullptr;
+    this->cantidadElementos = 0;
+    pthread_mutex_init(&this->mutexCola, NULL);
+}
+
+template<typename T>
+bool Cola<T>::estaVacia() {
+    return this->cantidadElementos == 0;
+}
+
+template<typename T>
+void Cola<T>::encolar(T dato) {
+    Nodo *nodo = new Nodo();
+    nodo->dato = dato;
+    nodo->siguienteNodo = nullptr;
+    //Bloqueo modificaciones
+    pthread_mutex_lock(&this->mutexCola);
+
+    if(!this->primerNodo){
+        this->primerNodo = nodo;
+    }else{
+        this->ultimoNodo->siguienteNodo = nodo;
+    }
+    this->ultimoNodo = nodo;
+    this->cantidadElementos++;
+    //Libero
+    pthread_mutex_unlock(&this->mutexCola);
+}
+
+template<typename T>
+T Cola<T>::desencolar() {
+    if(!this->cantidadElementos)
+        throw std::out_of_range("La cola ya está vacía");
+    //Bloqueo modificaciones
+    pthread_mutex_lock(&this->mutexCola);
+
+    Nodo *nodo = this->primerNodo;
+    T elemento = nodo->dato;
+    this->primerNodo = nodo->siguienteNodo;
+    delete nodo;
+    this->cantidadElementos--;
+    //Libero
+    pthread_mutex_unlock(&this->mutexCola);
+
+    return elemento;
+}
+
+template <typename T>
+Cola<T>::~Cola() {
+    Nodo *nodoTemp;
+    while(primerNodo != nullptr){
+        nodoTemp = primerNodo;
+        primerNodo = nodoTemp->siguienteNodo;
+        delete nodoTemp;
+    }
+    cantidadElementos = 0;
+    ultimoNodo = nullptr;
+}
+
+
+// Definir acá para qué tipo de datos aplica esta Cola
+template class Cola<std::string>;
+template class Cola<int>;
---
+++ src/Cola.h
@@ -0,0 +1,36 @@
+//
+// Created by nico on 16/10/19.
+//
+
+#ifndef PROYECTOTALLER_LUMINEGRO_COLA_H
+#define PROYECTOTALLER_LUMINEGRO_COLA_H
+
+#include "pthread.h"
+
+template <typename T>
+class Cola
+{
+public:
+    Cola();
+    bool estaVacia();
+    void encolar(T dato);
+    T desencolar();
+    ~Cola();
+private:
+    struct Nodo{
+        T dato;
+        Nodo *siguienteNodo;
+    };
+
+    Nodo *primerNodo;
+    Nodo *ultimoNodo;
+    int cantidadElementos;
+    pthread_mutex_t mutexCola;
+};
+//NOTA: Para utilizar esto, definir el tipo de dato con el que se quiera manejar en "Cola.cpp" (ver el final del mismo)
+//Ejemplo, quiero hacer una cola de doubles, declaro en "Cola.cpp" lo siguiente:
+//                                                          template class Cola<double>;
+//Esto es por algunas restricciones que tiene usar "template" y cómo separar interfaz de definición
+
+
+#endif //PROYECTOTALLER_LUMINEGRO_COLA_H
