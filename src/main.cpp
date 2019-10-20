6733e8bc3fc4b6b3d4895767a7422abdb5dff1b0
mariano <mlapenna79@hotmail.com>
2019-10-20 15:46:18 -0300

    Reestablezco el pedido de credenciales en cliente

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ int main (int argc, char** argv) {
             return EXIT_FAILURE;
         }
 
-        //ventanaClienteInicioSesion.pedirCredenciales();
+        ventanaClienteInicioSesion.pedirCredenciales();
 
         // INICIO - CODIGO DE EJEMPLO: la siguiente rutina simula cierta espera, da error de contraseña, pide
         // credenciales nuevamente y finalmente hace como que conecta. */
