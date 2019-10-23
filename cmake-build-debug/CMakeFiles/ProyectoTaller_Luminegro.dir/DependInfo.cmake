1d2eaa8b9f74f918547fdad09c629594d50c1203
JulioCastillo1 <juliofmed@gmail.com>
2019-10-22 23:35:19 -0300

    Agrego un par de lineas al cmake.

---
+++ cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/lib/pugixml/pugixml.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/lib/pugixml/pugixml.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/comunicacion/MensajeCliente.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/comunicacion/MensajeCliente.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/comunicacion/MensajeCredenciales.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/comunicacion/MensajeCredenciales.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/comunicacion/MensajeServidor.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/comunicacion/MensajeServidor.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/comunicacion/RecibidorMensajesCliente.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/comunicacion/RecibidorMensajesCliente.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/comunicacion/Socket.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/comunicacion/Socket.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoEnemigoAvanzando.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoEnemigoAvanzando.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoEnemigoParado.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoEnemigoParado.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugador.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugador.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorAgachado.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorAgachado.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorAvanzando.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorAvanzando.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorParado.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorParado.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorPateando.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorPateando.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorPegando.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorPegando.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/estados/EstadoJugadorSaltando.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/estados/EstadoJugadorSaltando.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/Encuadre.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/Encuadre.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/Imagen.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/Imagen.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/Renderizador.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/Renderizador.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/Textura.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/Textura.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/VentanaCliente.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/VentanaCliente.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/grafica/VentanaClienteInicioSesion.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/grafica/VentanaClienteInicioSesion.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/main.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/main.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/BarrilModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/BarrilModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/CajaModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/CajaModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/ControlEnemigosModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/ControlEnemigosModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/ControlJugadoresModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/ControlJugadoresModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/ControlObjetosModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/ControlObjetosModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/CuchilloModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/CuchilloModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/ElementoModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/ElementoModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/EnemigoModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/EnemigoModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/FondoModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/FondoModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/JugadorModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/JugadorModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/PersonaModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/PersonaModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/modelos/TuboModelo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/modelos/TuboModelo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/Cliente.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/Cliente.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/Cola.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/Cola.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/ColaMensajesCliente.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/ColaMensajesCliente.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/ConfigManager.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/ConfigManager.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/GestorThreads.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/GestorThreads.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/Logger.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/Logger.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/varios/Servidor.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/varios/Servidor.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaEnemigo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaEnemigo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaFondo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaFondo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaFondoCielo.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaFondoCielo.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaFondoEdificios.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaFondoEdificios.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaFondoTerreno.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaFondoTerreno.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaGeneral.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaGeneral.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaJugador.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaJugador.cpp.o"
+  "/home/julio/CLionProjects/ProyectoTaller-Luminegro/src/vistas/VistaObjeto.cpp" "/home/julio/CLionProjects/ProyectoTaller-Luminegro/cmake-build-debug/CMakeFiles/ProyectoTaller_Luminegro.dir/src/vistas/VistaObjeto.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/pugixml"
+  "../src"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
